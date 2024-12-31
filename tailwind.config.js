957d89afad10294027ea764c0f7caf68da2f19fc
Rahim Adnan <hafizrahim3011@gmail.com>
2024-12-31 07:04:08 +0500

    Do basic setup for project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js,css}",             
+    "./src/**/*.{html,js,ejs,jsx,ts,tsx}",    
+    "./src/views/**/*.ejs",                  
+    "./src/views/partials/**/*.ejs",        
+    "./src/views/layouts/**/*.ejs"           
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
