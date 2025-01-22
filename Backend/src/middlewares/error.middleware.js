4d83a723e2f5a16aeae5bacc57e68d367b3aa088
Rahim Adnan <hafizrahim3011@gmail.com>
2025-01-22 05:26:54 +0500

    setup folders

--- Backend/src/middlewares/error.middleware.js
+++ Backend/src/middlewares/error.middleware.js
@@ -1,5 +1,5 @@
 const errorHandler = async (err, req, res, next) => {
   let {status = 500, message = "Something went wrong"} = err;
-  res.status(status).render("../views/pages/error.ejs", {message});
+  res.status(status).send({message});
 };
 export {errorHandler};
