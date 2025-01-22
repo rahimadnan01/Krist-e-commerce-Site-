771f8da9f4abb0c0ce9bd4264a92ac2a495f45bb
Rahim Adnan <hafizrahim3011@gmail.com>
2025-01-22 11:52:36 +0500

    add cart remove feuture

--- Backend/src/routes/cart.routes.js
+++ Backend/src/routes/cart.routes.js
@@ -1,8 +1,12 @@
 import {Router} from "express";
-import {createCart, showCart} from "../controllers/cart.controller.js";
+import {
+  createCart,
+  showCart,
+  removeCart,
+} from "../controllers/cart.controller.js";
 import {verifyJWT} from "../middlewares/auth.middelware.js";
 const router = Router();
 
 router.route("/cart").post(verifyJWT, createCart).get(verifyJWT, showCart);
-
+router.route("/remove-cart/:productId").delete(verifyJWT, removeCart);
 export default router;
